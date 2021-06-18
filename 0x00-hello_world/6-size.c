6f50bb01a9cba44013d2dc29a4f74adf3c47fcb0
Juan Osorio <3387@holbertonschool.com>
2021-06-17 19:59:03 -0500

    add C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,16 @@
 int main(void)
 
 {
+
 char chartype;
 int inttype;
 long int longinttype;
-long long int longlonginttypee;
+long long int longlonginttype;
 
-printf("Size of char: %zu bytes\n") sizeof(chartype);
-printf("size of int: %zu bytes\n") sizeof(inttype);
-printf("size of long int: %zu bytes\n") sizeof(loginttype);
-printf("size of long long int: %zu bytes\n") sizeof(longlonginttypee);
+printf("Size of char: %lu bytes\n") sizeof(chartype);
+printf("size of int: %lu bytes\n") sizeof(inttype);
+printf("size of long int: %lu bytes\n") sizeof(loginttype);
+printf("size of long long int: %lu bytes\n") sizeof(longlonginttype);
 return (0);
  
 }
