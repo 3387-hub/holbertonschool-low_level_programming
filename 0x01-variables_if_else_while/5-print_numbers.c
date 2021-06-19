98cc1563f152792c3ffac3a3613732075428f21b
Juan Osorio <3387@holbertonschool.com>
2021-06-18 20:59:47 -0500

    I Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Is the principal function
+ * Return: (0) always is 0 (success)
+ */
+
+int main(void)
+
+{
+
+int i = 48;
+
+while (i <= 57)
+{
+	putchar(i);
+	i++;
+}
+putchar(10);
+return (0);
+}
