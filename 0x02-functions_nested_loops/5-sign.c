5ef5a15a56b21e86fd937e92732d78f5e8eb2bde
Juan Osorio <3387@holbertonschool.com>
2021-06-22 09:35:21 -0500

    I Write a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int print_sign(int n)
+{
+	if(n > 48)
+	{
+		_putchar('+');
+			return (1);
+	}
+
+	else if(n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+
+	else
+	{
+		 _putchar('-');
+		 return (-1);
+	}
+}
