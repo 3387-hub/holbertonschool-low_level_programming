b6d2e3f129fa0a3de28404721c2519174b138af4
Juan Osorio <3387@holbertonschool.com>
2021-06-22 18:36:05 -0500

    i solve the betty's problems

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- *_abs: This function we say whats the absolute value of the integer.
- *description - This is a script that prints the absolute value of an integer
+ * _abs - This function we say whats the absolute value of the integer.
  *@r: This variable is an aleatory character
  *Return: (r) if the variable is >= 0, (r*0) if the variable is < than 0
  */
