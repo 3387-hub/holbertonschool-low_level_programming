9c4c7238ebf7fe5c5bb095fc1cbb20043d39a8c9
Juan Osorio <3387@holbertonschool.com>
2021-06-22 18:27:43 -0500

    i solve the betty's problems

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
 
+/**
+ *print_sign - This function pritns +,- or 0 if n is +,- or 0
+ *@n: This variable give us a integer character.
+ *Return: (1) for numbers +, (0) for 0 and (-1) for numbers -
+ */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar('+');
+	return (1);
 	}
 
 
-	else if(n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
@@ -17,8 +22,8 @@ int print_sign(int n)
 
 	else
 	{
-		 _putchar(45);
-		 return (-1);
+		_putchar(45);
+		return (-1);
 	}
 }
 
