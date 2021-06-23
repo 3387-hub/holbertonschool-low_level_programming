a062f7b35564d9059b0fa64df0df20e4a8f0719e
Juan Osorio <3387@holbertonschool.com>
2021-06-22 21:27:23 -0500

    I Write a function that prints the last digit of a number.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _print_last_digit - This function print the last digit of the p
+ *print_last_digit - This function print the last digit of the p
  *@p: is the number it wich we want to have the last digit
  *Return: always is the last digit of our variable
  */
 
 int print_last_digit(int p)
 {
-	if ( p < 0)
+	if (p < 0)
 	{
 		p = (p * -1);
 	}
