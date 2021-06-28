147a496599b194b3289f682e68d43d28c175b2d6
Juan Osorio <3387@holbertonschool.com>
2021-06-28 18:22:32 -0500

    I add README.md, holberton.h and first task 0-98 Batter st

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int e, i;
+	int r;
+
+	for (e = 1; e <= 9; e++)
+	{
+		for (i = 1; i <= 9; i++)
+		{
+
+			for (r= e * i; r >= e; r++)
+
+		}
+		_putchar(44);
+		_putchar(10);
+	}
+
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - This function  takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ * @n: This pointer variable takes a pointer to an int as parameter and update
+ * the value it points to to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
