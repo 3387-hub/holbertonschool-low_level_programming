27dd9b525af838a4773f68bed6616b66384d4d87
Juan Osorio <3387@holbertonschool.com>
2021-06-29 19:07:21 -0500

    I write a function that reverse the sring

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - This function  prints a string, invert values of an array
+ * @s: This variable give us the string to use.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	b = a;
+	for (c = b - 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+}
