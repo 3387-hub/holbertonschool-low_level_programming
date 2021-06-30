835c539aa4018f64b66c71be41f537f8c69f3d6c
Juan Osorio <3387@holbertonschool.com>
2021-06-29 21:00:16 -0500

    I write a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * rev_string - This function reverse a string
+ *@s: is the variable which contain the string
+ */
+void rev_string(char *s)
+{
+
+	int a;
+	int b;
+	int i1;
+	int i2;
+	int tmp1;
+	int tmp2;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	b = a;
+	for (i1 = b - 1, i2 = 0; i1 != i2; i1--, i2++)
+	{
+		tmp1 = s[i2];
+		tmp2 = s[i1];
+		s[i2] = tmp2;
+		s[i1] = tmp1;
+	}
+
+}
