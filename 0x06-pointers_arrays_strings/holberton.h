a3cd2629dc19b0134e4e83d6182633097c83c255
Juan Osorio <3387@holbertonschool.com>
2021-07-01 21:43:54 -0500

    0x06. C - More pointers, arrays and strings
        Foundations > Low-level programming & Algorithm > Hatching out
        By Julien Barbier, co-founder & CEO at Holberton School
        Ongoing project - started 07-01-2021, must end by 07-03-2021 (in 1 day) - you're done with 0% of tasks.
        Checker will be released at 07-02-2021 11:00 AM
        QA review fully automated.
    
    Requirements
    General
    
        Allowed editors: vi, vim, emacs
        All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
        All your files should end with a new line
        A README.md file, at the root of the folder of the project is mandatory
        Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
        You are not allowed to use global variables
        No more than 5 functions per file
        You are not allowed to use the standard library. Any use of functions like printf, puts, etc… is forbidden
        You are allowed to use _putchar
        You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
        In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
        The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called holberton.h
        Don’t forget to push your header file

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
+char *string_toupper(char *);
+#endif
