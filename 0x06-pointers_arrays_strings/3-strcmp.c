41c3a5fc845fac7d838d6aeb2d6317809db073d3
Juan Osorio <3387@holbertonschool.com>
2021-07-02 15:10:30 -0500

    I write a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,30 +8,18 @@
 int _strcmp(char *s1, char *s2)
 
 {
-	int size1;
-	int size2;
 	int indx;
 
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-	{
-	}
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-	{
-	}
-	if (size1 > size2)
-	{
-		return (15);
-	}
-	else if (size1 < size2)
-	{
-		return (-15);
-	}
 	for (indx = 0; s1[indx] != '\0' && s2[indx] != '\0'; indx++)
 	{
-		if (s1[indx] != s2[indx])
+		if (s1[indx] > s2[indx])
 
 		{
-			return (-15);
+			return (1);
+		}
+		else if (s1[indx] < s2[indx])
+		{
+			return (-1);
 		}
 	}
 	return (0);
