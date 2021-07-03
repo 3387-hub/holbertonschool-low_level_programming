8d2f5aa8013c2b60ac0b14aa592e1cd963ceae82
Juan Osorio <3387@holbertonschool.com>
2021-07-02 22:01:10 -0500

    I wrote a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,31 @@
 #include "holberton.h"
-/**
- * cap_string - This function capitalizes all words of a string
- *@s: This is the main string
- *
- *Return: return always is s.
- */
-
-char *cap_string(char *s)
-{
-	char x[] = {' ', '\t', '\n', ',', ';', '.',
-		    '!', '?', '"', '(', ')',
-		    '{', '}', '\0'};
-	int i;
-	int i2;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 97 && s[i] <= 122)
-		{
-			s[i] = s[i] - 32;
-		}
-		for (i2 = 0; x[i2] != '\0'; i2++)
-		{
-			if (s[i] == x[i2])
-			{
-				if (s[i + 1] >= 97 && s[i + 1] <= 122)
-				{
-					s[i + 1] = s[i + 1] - 32;
-				}
-			}
-		}
-	}
-	return (s);
-}
+  /**
+  * *cap_string - Function that capitalizes all words of a string
+  * Return: String of type char
+  * @s: String to be reversed
+  */
+ char *cap_string(char *s)
+ {
+ char x[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{',
+         '}'};
+ int i;
+ int j;
+   for (j = 0; x[j] != '\0'; j++)
+ {
+         if (s[i] >= 97 && s[i] <= 122)
+         {
+                 s[i] = s[i] - 32;
+         }
+         for (i = 0; s[i] != '\0'; i++)
+         {
+                 if (s[i] == x[j])
+                 {
+                         if (s[i + 1] >= 97 && s[i + 1] <= 122)
+                         {
+                                 s[i + 1] = s[i + 1] - 32;
+                         }
+                 }
+         }
+ }
+ return (s);
+ }
