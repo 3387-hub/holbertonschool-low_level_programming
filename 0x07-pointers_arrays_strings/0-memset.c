ba4f510455f35eb330130349b00d15cb1c129fa9
Juan Osorio <3387@holbertonschool.com>
2021-07-05 13:35:29 -0500

    I write a function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_memset - This function fills memory with a constant byte.
+ *@s: Is the pointer to edit with another pointer
+ *@b: Is the value of replace in each place of s
+ *@n: Is the number of place that i need to replace in s.
+ *Return: always is s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int e;
+	char value = b;
+	char *p = s;
+
+	for (e = 0; e <= n; e++)
+	{
+		p[e] = value;
+	}
+
+	return (s);
+
+}
