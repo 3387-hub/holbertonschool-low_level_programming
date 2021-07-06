38cbdf8ace144f598084ee1cfe6ea38286140be7
Juan Osorio <3387@holbertonschool.com>
2021-07-05 21:20:44 -0500

    I wrote a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strstr - This function locate a substring
+ *@haystack: string where we search the substring
+ *@needle: substring that i need find in haystack
+ *Return: &needle[i] if there are coincidence NULL if not.
+ */
+char *_strstr(char *haystack, char *needle)
+
+{
+
+	int e;
+	int i;
+
+	for (e = 0; haystack[e] != '\0'; e++)
+	{
+
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (haystack[e - 1] == needle[i - 1])
+			{
+				return (&needle[i]);
+			}
+		}
+	}
+	return (NULL);
+
+
+}
