8b06e12b8e910ad0bb7a45e2891859a8e5fca272
Juan Osorio <3387@holbertonschool.com>
2021-07-08 15:26:27 -0500

    I wrote a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This program prints its name, folloed by a new line.
+ *@argc: This is the counter that save enough memory to the strings that
+ *will be received
+ *@argv: This is the pointer who point the string received.
+ *Return: always is (0)
+ */
+
+int main (int argc __attribute__((unused)), char* argv [])
+
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
