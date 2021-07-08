b10b7bbaac1823ba86dfa1ee0721fba064e890e4
Juan Osorio <3387@holbertonschool.com>
2021-07-08 15:40:32 -0500

    I wrote a program that prints the number of arguments  passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This program prints the number of arguments passed into it
+ *@argc: This is the counter that save enough memory to the strings that
+ *will be received.
+ *@argv: This is the pointer who point the string received.
+ *Return: always is (0)
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+
+{
+	printf("%d\n", argc);
+	return (0);
+}
