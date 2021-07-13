be5fcf1cf9cf8cfd321c05b9b150eb77da863644
Juan Osorio <3387@holbertonschool.com>
2021-07-13 17:19:36 -0500

    I wrote a function that creates an array of chars, and initializates it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <limits.h>
+/**
+ *create_array - This function creates an array of chars, and initializes it
+ *with a specific char.
+ *@size: is the size of my string.
+ *@c: is the character which my function initialices.
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc (sizeof(char) * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		str[i] = c;
+	}
+	str[i] = '\0';
+	return (str);
+}
