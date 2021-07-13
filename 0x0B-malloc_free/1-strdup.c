a7b14d77d6c33edfe3f1a34870d39ac086ef12e7
Juan Osorio <3387@holbertonschool.com>
2021-07-13 18:36:17 -0500

    I wrote a function that create space in the memory with malloc for a copy from the string given.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strdup - This function return a pointer to a newly allocated space in
+ *memory, which contains a copy of the string given as a parameter.
+ *@str: This is the string that i'm going to copy.
+ *Return: always return pointer.
+ */
+char *_strdup(char *str)
+
+{
+	char *pointer;
+	int i;
+	int j;
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+	}
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	pointer = malloc(sizeof(char) * i + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i + 1; j++)
+	{
+		pointer[j] = str[j];
+	}
+/*This assignment below it's why i need add the '\0' to the end of the string*/
+	pointer[i] = '\0';
+	return (pointer);
+}
