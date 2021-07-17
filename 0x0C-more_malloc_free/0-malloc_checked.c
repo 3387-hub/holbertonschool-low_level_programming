eab1898bbebbaeeeedc72eaf6b861b5398e1530b
Juan Osorio <3387@holbertonschool.com>
2021-07-17 13:29:26 -0500

    I wrote a function that allocates memory using malloc and return 98 in case of error

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - This function allocates memory using malloc.
+ *@b: This is the quantify to malloc using to allocate memory.
+ *Return: fi there is no errors return my pointer, if there is error
+ *return (98)
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *s;
+
+	s = malloc(sizeof(int) * b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+
+}
