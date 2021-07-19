9d4e51b984e70cf5b4c4b0b760f722ba910ce128
Juan Osorio <3387@holbertonschool.com>
2021-07-19 16:10:13 -0500

    I wrote a program that use the __FILE__ macro to prints the name of the current file.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program prints the name of the current file.
+ *Return: always is (0).
+ *
+ */
+
+int main(void)
+
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
