22ba233ac0abee311c9289aba620859eaa9257fd
Juan Osorio <3387@holbertonschool.com>
2021-07-19 18:33:14 -0500

    I create a new type of structure.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - It's a new type of structure
+ *@name: A new type of char.
+ *@age: A new type of float.
+ *@owner: A new type of pointer char.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+#endif
