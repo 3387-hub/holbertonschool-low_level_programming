d2c32157790084ab671cf57885cfbe1636145987
3387-hub <3387@holbertonschool.com>
2021-07-22 23:34:44 +0000

    Another 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  *each element of an array.
  *@array: This is the  array where i find the elements.
  *@size: This is the size of the array.
- *@action: This is my pointer to point the function.
+ *@action: This is my pointer to point the functionn.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 
