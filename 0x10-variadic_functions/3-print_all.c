41918994515cff11fab291525504a9168915f9eb
3387-hub <3387@holbertonschool.com>
2021-07-24 23:36:48 +0000

    I wrote a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
  #include <stdarg.h>
 /**
  * print_all - This function prints anything.
- *@format:
+ *@format: Arguments recieved.
  */
 void print_all(const char * const format, ...)
 
@@ -15,34 +15,34 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 	if (format)
+	{
+		while (format[i])
 		{
-			while (format[i])
-				{
-					switch (format[i])
-						{
-						case 'c':
-							printf("%s%c", space, va_arg(ap, int));
-							break;
-						case 'i':
-							printf("%s%d", space, va_arg(ap, int));
-							break;
-						case 'f':
-							printf("%s%f", space, va_arg(ap, double));
-							break;
-						case 's':
-							s = va_arg(ap, char*);
-							if (s == NULL)
-								s = "(nil)";
-							printf("%s%s", space, s);
-							break;
-						default:
-							i++;
-							continue;
-						}
-					space = ", ";
-					i++;
-				}
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", space, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%d", space, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", space, va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char*);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", space, s);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			space = ", ";
+			i++;
 		}
+	}
 	printf("\n");
 	va_end(ap);
 }
