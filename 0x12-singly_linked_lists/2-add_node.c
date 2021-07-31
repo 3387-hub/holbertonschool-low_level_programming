f1b4e304de755e3f64c8b64b4b1e51470c419791
3387-hub <3387@holbertonschool.com>
2021-07-31 00:26:33 +0000

    I wrote a function that adds a new node at the beginning of a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *add_node - This function adds a new node at the beginning of a list_t list.
+ *@head: This is my double pointer to a pointer to a content of head
+ *@str: This is the str to assing the new head.
+ *Return: Always is *head.
+ */
+list_t *add_node(list_t **head, const char *str)
+
+{
+	list_t *pointernewhead;
+	int i = 0;
+
+
+	pointernewhead = malloc(sizeof(list_t));
+		if (pointernewhead == NULL)
+		{
+			return (NULL);
+		}
+
+		while (str[i] != '\0')
+		{
+			i++;
+		}
+	pointernewhead->str = strdup(str);
+	pointernewhead->len = i;
+	pointernewhead->next = (*head);
+	(*head) = pointernewhead;
+	return (*head);
+
+}
