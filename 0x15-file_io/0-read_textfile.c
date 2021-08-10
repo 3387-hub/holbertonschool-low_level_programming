fede8df2da1f172b86e22275f375d004a494108f
PablitoOsorix <3387@holbertonschool.com>
2021-08-09 21:18:04 -0500

    I wrote a function that reads a text line and prints it to the POSIX standart output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,11 @@
 #include <fcntl.h>
 #include "main.h"
 /**
- *This function reads a text file and prints it to the POXIS standard output.
+ *read_textfile - This function reads a text file and prints it to the
+ *POXIS standard output.
  *@filename: This is the file that i'm going to cp.
  *@letters: This is the size of the filename.
- *Return:
+ *Return: if there isn't a error it return fd1.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 
@@ -18,14 +19,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buff[4000];
 	int fd = 0, fd1 = 0, fd2 = 0;
 
-	if(filename == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
 
 	fd = open(filename, O_RDONLY);
 
-	if(fd == -1)
+	if (fd == -1)
 	{
 		return (0);
 	}
@@ -36,13 +37,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	fd2= write(STDOUT_FILENO, buff, fd1);
+	fd2 = write(STDOUT_FILENO, buff, fd1);
 
 	if (fd2 == -1)
 	{
 		return (0);
 	}
-	close (fd);
+	close(fd);
 
-	return(fd1);
+	return (fd1);
 }
