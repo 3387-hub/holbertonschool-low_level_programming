5d9102bbc1f100cccce87d9d5a5dd3246b75a99c
pablito <3387@holbertonschool.com>
2021-10-05 14:00:09 -0500

    I add a function that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Is the principal function that print Holberton
+ * Return: (0) Always 0 is (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
