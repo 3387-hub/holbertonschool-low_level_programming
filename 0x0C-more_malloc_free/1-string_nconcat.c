a52df43d6c22aca8ebb8be66a9dada1f6e6bccb1
pablito <3387@holbertonschool.com>
2021-10-28 20:55:55 -0500

    I added my 1-string concat.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (k = 0; s1[k] != '\0'; k++)
-	;
+	for (k = 0; s1[k] != '\0'; k++);
 
-	for (l = 0; s2[l] != '\0'; l++)
-	;
+	for (l = 0; s2[l] != '\0'; l++);
 
 	if (n >= l)
 		n = l;
