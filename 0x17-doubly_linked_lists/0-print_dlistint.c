cb948b3543263329a8abf6f133be0017596cbb73
3387-hub <3387@holbertonschool.com>
2022-01-06 15:50:40 -0500

    I write a function that prints the content of all nodes.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,27 +5,26 @@
 #include <stddef.h>
 
 /**
- * print_dlistint - This function pritnrs all the elements 
- * of a dlistint_t
- * 
- * @dlistint_t: This is a double linked list which we print all elemt.
- * @return a size_t with the number of nodes.
- */
+*print_dlistint - This function pritnrs all the elements
+*of a dlistint_t
+*@h: This is a double linked list which we print all elemt.
+*Return: a size_t with the number of nodes.
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    while (h)
-    {
-        if(h->next != NULL)
-        {
-            printf("%d", h->n);
-            printf("\n");
-        }
-        h = h->next;
-        counter++;
+	while (h)
+	{
+		if (h->next != NULL)
+		{
+			printf("%d", h->n);
+			printf("\n");
+		}
+		h = h->next;
+		counter++;
 
-    }
-    return (counter);
+	}
+	return (counter);
 }
