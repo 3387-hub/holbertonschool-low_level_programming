78ed1b0a44605c447dca12f8f8a32d2ad63061a6
3387-hub <3387@holbertonschool.com>
2022-01-06 15:58:57 -0500

    I write a function that prints the content of all nodes.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -25,6 +25,12 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		counter++;
 
+		if (h ->prev != NULL)
+		{
+			printf("%d", h->n);
+		}
+		h = h->next;
+		counter++;
 	}
 	return (counter);
 }
