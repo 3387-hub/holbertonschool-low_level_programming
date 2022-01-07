d311719c4350b048ed2a0628c9fb41cecf491e03
3387-hub <3387@holbertonschool.com>
2022-01-06 23:03:04 -0500

    I write a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,33 +15,39 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *mvptr;
-	unsigned int j = 0;
+	dlistint_t *current = *h;
+	unsigned int count = 0;
 
+	if (!h)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node->next = (*h);
-		*h = new_node;
-		return (new_node);
+		return (add_dnodeint(h, n));
 	}
-	new_node->next = NULL;
-	mvptr = *h;
-	while (mvptr->next && j < (idx - 1))
+	while (current)
 	{
-		mvptr = mvptr->next;
-		j++;
+		if (count == idx)
+		{
+			new_node->n = n;
+			new_node->prev = current->prev;
+			new_node->next = current;
+			current->prev->next = new_node;
+			current->prev = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		count = count + 1;
 	}
-	if (j < (idx - 1))
+	if (count == idx)
 	{
-		return (NULL);
+		return (add_dnodeint_end(h, n));
 	}
-	new_node->next = mvptr->next;
-	mvptr->next = new_node;
-	return (new_node);
+	return (NULL);
 }
