f404caf58719f0deaa3d7b43af0685bf659bb60d
PabloOsorix <3387@holbertonschool.com>
2022-01-28 13:48:25 -0500

    :heavy_plus_sign: add prototype of hash_table_get

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,4 +36,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *new_node_fc(const char *key, char *cp_value);
 int key_update(hash_node_t *tmp, char *cp_key, char *cp_value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif
